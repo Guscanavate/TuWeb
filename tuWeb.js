1548878dddf3254b6abea276f37ccdd348fb5a59
Gustavo Canavate <gustavocanavate@gmail.com>
2022-06-09 18:56:31 -0300

    first commit

---
+++ tuWeb.js
@@ -0,0 +1,20 @@
+// Crear nuevos elementos para agregar a un diseño web modelo
+
+let nuevoNegocio = document.createElement ('h2');
+nuevoNegocio.innerHTML='<h2>SUPERKIOSKO</H2>';
+let pos1 = document.getElementById ('negocio');
+pos1.append (nuevoNegocio);
+
+let nuevaMarca = document.createElement ('span');
+nuevaMarca.innerHTML='<span>  SUPERKIOSKO</span>';
+let pos2 = document.getElementById ('marca');
+pos2.append (nuevaMarca);
+
+
+
+
+
+
+
+
+
